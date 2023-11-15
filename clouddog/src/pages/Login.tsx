905eeff43bfb4340251621024de15528a2e2eb12
yeeZinu <doglife222@gmail.com>
2023-11-15 19:50:36 +0900

    feat(#4): 로그인 버튼 스타일 변경 및 나머지 구현

--- clouddog/src/pages/Login.tsx
+++ clouddog/src/pages/Login.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+import BackImg from '@/styles/login.module.css';
 import { auth } from '@/apis/instance/firebase';
 import { useRouter } from 'next/router';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
@@ -30,11 +32,112 @@ const Login = () => {
     }
   };
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={googleLogin}>구글 로그인</button>
-    </div>
+    <TotalContainer>
+      <ExplainContainer>
+        <DogImg></DogImg>
+        <TitleText>뭉개뭉개</TitleText>
+        <ExplainText>
+          뭉개뭉개와 함께 댕댕이와의 추억을 기록하고 헤어짐을 새롭게
+          재해석해보세요!
+        </ExplainText>
+      </ExplainContainer>
+      <GoogleLoginBtn onClick={googleLogin}>
+        <GoogleLogo>
+          <Image
+            src={'/Google__G__Logo 1.png'}
+            alt={'Google_Logo'}
+            fill
+          ></Image>
+        </GoogleLogo>
+        <GoogleText>구글 로그인</GoogleText>
+      </GoogleLoginBtn>
+      <Image
+        className={BackImg.img}
+        src={'/LoginBackImg.png'}
+        alt={'LoginBackImg'}
+        fill
+      ></Image>
+    </TotalContainer>
   );
 };
 
 export default Login;
+
+const TotalContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+
+  width: calc(100vw - (100vw - scrollbarWidth));
+  height: 100vh;
+
+  gap: 20vh;
+`;
+
+const ExplainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 2.3vh;
+`;
+
+const DogImg = styled.div`
+  width: 9.5vw;
+  height: 9.5vw;
+
+  border-radius: 50%;
+
+  background-color: #f3f3f3;
+`;
+
+const TitleText = styled.div`
+  font-size: 2vw;
+  font-weight: bold;
+`;
+const ExplainText = styled.div`
+  width: 20.88vw;
+  text-align: center;
+  font-size: 1vw;
+  font-weight: bold;
+`;
+
+const GoogleLoginBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+
+  width: 28.4167vw;
+  height: 7.7192vh;
+  gap: .5vw;
+
+  border: 2px solid black;
+  border-radius: 0.7vw;
+
+  background-color: white;
+
+  transition: 0.2s;
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    transition: 0.2s;
+    transform: translateY(2px);
+  }
+`;
+
+const GoogleLogo = styled.div`
+  position: relative;
+  width: 2vw;
+  height: 4vh;
+`;
+
+const GoogleText = styled.div`
+  text-align: center;
+  font-size: 1.5vw;
+  font-weight: bold;
+`;
