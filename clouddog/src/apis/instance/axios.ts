c4515670327161dcbdc4cc8dc2ff09f2f74cc4b4
yeeZinu <doglife222@gmail.com>
2023-11-15 17:00:04 +0900

    build: axios 설정 수정

--- clouddog/src/apis/instance/axios.ts
+++ clouddog/src/apis/instance/axios.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const accessToken = localStorage.getItem('accessToken');
+// const accessToken = localStorage.getItem('accessToken');
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-    'Content-Type': 'application/json',
-    // 'Access-Control-Allow-Origin': 'http://localhost:3000',
-    // 'Access-Control-Allow-Credentials': true,
-  },
+  //   headers: {
+  //     Authorization: `Bearer ${accessToken}`,
+  //     'Content-Type': 'application/json',
+  // 'Access-Control-Allow-Origin': 'http://localhost:3000',
+  // 'Access-Control-Allow-Credentials': true,
+  //   },
 });
